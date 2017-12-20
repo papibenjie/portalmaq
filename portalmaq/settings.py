09b437d5fb4a5116f4a96ad7813991b85c352a2d
Marc-Antoine Quessy <papibenjie@gmail.com>
2017-12-20 15:49:50 -0500

    allo

--- portalmaq/settings.py
+++ portalmaq/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,9 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "mygist/static"),
 ]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR + "/staticfiles"
+
+django_heroku.settings(locals())
